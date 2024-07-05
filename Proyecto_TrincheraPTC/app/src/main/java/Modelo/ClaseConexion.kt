14c27fde6eae8acf9682a71ee3f85218be13c5f8
William Campos <20230079@ricaldone.edu.sv>
2024-07-05 16:13:20 -0600

    Se finalizo con el login y se adelanto un poco con la pantalla de registro

---
+++ Proyecto_TrincheraPTC/app/src/main/java/Modelo/ClaseConexion.kt
@@ -0,0 +1,22 @@
+package Modelo
+
+import java.lang.Exception
+import java.sql.Connection
+import java.sql.DriverManager
+
+class ClaseConexion {
+    fun cadenaConexion(): Connection?{
+        try {
+            val url="jdbc:oracle:thin:@192.168.1.26:1521:xe"
+            val user="WILLIAM_DEVELOPER"
+            val password="123456"
+
+            val connection= DriverManager.getConnection(url, user, password)
+
+            return connection
+        }catch (e: Exception){
+            println("este es el error:$e")
+            return null
+        }
+    }
+}
--- Proyecto_TrincheraPTC/app/src/main/java/equipo/ptc/proyecto_trincheraptc/Login.kt
+++ Proyecto_TrincheraPTC/app/src/main/java/equipo/ptc/proyecto_trincheraptc/Login.kt
@@ -1,10 +1,20 @@
 package equipo.ptc.proyecto_trincheraptc
 
+import Modelo.ClaseConexion
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.security.MessageDigest
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +26,60 @@ class Login : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        //1- Mandamos a traer a todos los elementos de la vista
+        val txtCorreoElectronico = findViewById<EditText>(R.id.txtCorreoElectronico)
+        val txtContrasena = findViewById<EditText>(R.id.txtContrasena)
+        val btnEntrar = findViewById<Button>(R.id.btnEntrar)
+        val btnRegistrarse = findViewById<Button>(R.id.btnRegistrarse)
+
+        fun hashSHA256(input: String): String {
+            val bytes = MessageDigest.getInstance("SHA-256").digest(input.toByteArray())
+            return bytes.joinToString("") { "%02x".format(it) }
+        }
+
+        //2- Se programa los botones
+        btnEntrar.setOnClickListener {
+
+
+            val pantallaPrincipal = Intent(this, MainActivity::class.java)
+            GlobalScope.launch(Dispatchers.IO) {
+                //2- Creo una variable que contenga un PrepareStatement
+                //Se hace un select where el correo y la contraseña sean iguales a
+                //los que el usuario escribe
+                //Si el select encuentra un resultado es por que el usuario y contraseña si están
+                //en la base de datos, si se equivoca al escribir algo, no encontrará nada el select
+                val objConexion = ClaseConexion().cadenaConexion()
+
+                val contrasenaEncriptada = hashSHA256(txtContrasena.text.toString())
+
+                val verificarUsuario = objConexion?.prepareStatement("SELECT * FROM USUARIOS3PTC WHERE correoElectronico = ? AND contrasena = ?")!!
+                verificarUsuario.setString(1, txtCorreoElectronico.text.toString())
+                verificarUsuario.setString(2, contrasenaEncriptada)
+                val resultado = verificarUsuario.executeQuery()
+                if (resultado.next()) {
+                    withContext(Dispatchers.Main) {
+                        startActivity(pantallaPrincipal)
+                    }
+
+                } else {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(
+                            this@Login,
+                            "Usuario o contraseña incorrectos",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+
+
+        }
+
+        //Botón para ir a la pantalla de registrarse
+        btnRegistrarse.setOnClickListener {
+            val pantallaRegistrarse = Intent(this, Register::class.java)
+            startActivity(pantallaRegistrarse)
+        }
     }
+
 }
