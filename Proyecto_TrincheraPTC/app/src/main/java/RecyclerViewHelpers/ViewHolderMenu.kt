5821b1f2f03119e3da6285b4480710afbcf12900
Ricardo <20230332@ricaldone.edu.sv>
2024-07-06 22:05:22 -0600

    menu principal

--- Proyecto_TrincheraPTC/app/src/main/java/RecyclerViewHelpers/ViewHolderMenu.kt
+++ Proyecto_TrincheraPTC/app/src/main/java/RecyclerViewHelpers/ViewHolderMenu.kt
@@ -5,6 +5,6 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import equipo.ptc.proyecto_trincheraptc.R
 
-class ViewHolderSopas(view: View): RecyclerView.ViewHolder(view) {
-    val txtNombreCard = view.findViewById<TextureView>(R.id.txtnombreCard)
+class ViewHolderMenu(view: View): RecyclerView.ViewHolder(view) {
+    val txtNombreCard = view.findViewById<TextureView>(R.id.txtCardMenu)
 }
---
+++ Proyecto_TrincheraPTC/app/src/main/java/equipo/ptc/proyecto_trincheraptc/Menu_Principal_CardActivity.kt
@@ -0,0 +1,20 @@
+package equipo.ptc.proyecto_trincheraptc
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Menu_Principal_CardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu_principal_card)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
