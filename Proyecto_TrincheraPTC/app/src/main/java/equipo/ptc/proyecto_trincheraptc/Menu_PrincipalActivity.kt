aebbfc8f9e3a71c5d712e3a082cc0d0773fcd874
Gabriel Alexander <gaboquintana10@gmail.com>
2024-07-08 00:09:18 -0600

    Elimine interferencias en menu

--- Proyecto_TrincheraPTC/app/src/main/java/equipo/ptc/proyecto_trincheraptc/Menu_PrincipalActivity.kt
+++ Proyecto_TrincheraPTC/app/src/main/java/equipo/ptc/proyecto_trincheraptc/Menu_PrincipalActivity.kt
@@ -20,18 +20,10 @@ class Menu_PrincipalActivity : AppCompatActivity() {
         }
 
            //Cambiar de pantalla entre vector
-
         val imgCarrito = findViewById<ImageView>(R.id.imgCarrito)
         imgCarrito.setOnClickListener {
                 val pantallaLogin = Intent(this, Menu_PrincipalActivity::class.java)
                 startActivity(pantallaLogin)
-
-                val imgsopas = findViewById<ImageView>(R.id.imgsopas)
-            imgsopas.setOnClickListener {
-                    val pantallaLogin = Intent(this, Menu_PrincipalActivity::class.java)
-                    startActivity(pantallaLogin)
-
-                }
             }
         }
     }
