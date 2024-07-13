977e439d9173bed7fc43a277bb72a6a49b7840d8
Santiago <20200148@ricaldone.edu.sv>
2024-07-12 19:32:06 -0600

    Mod layouts y botón de registrarse

--- Proyecto_TrincheraPTC/app/src/main/java/equipo/ptc/proyecto_trincheraptc/Login.kt
+++ Proyecto_TrincheraPTC/app/src/main/java/equipo/ptc/proyecto_trincheraptc/Login.kt
@@ -3,6 +3,7 @@ package equipo.ptc.proyecto_trincheraptc
 import Modelo.ClaseConexion
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -11,6 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ReportFragment.Companion.reportFragment
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -32,6 +34,12 @@ class Login : AppCompatActivity() {
         val txtContrasena = findViewById<EditText>(R.id.txtContrasena)
         val btnEntrar = findViewById<Button>(R.id.btnEntrar)
         val btnRegistrarse = findViewById<Button>(R.id.btnRegistrarse)
+        val btnGoogle = findViewById<Button>(R.id.btngoogle)
+
+        if (txtCorreoElectronico == null || txtContrasena == null || btnEntrar == null || btnRegistrarse == null || btnGoogle == null) {
+            Log.e("Login", "Uno o más elementos de la interfaz no se encuentran en el layout.")
+            return
+            }
 
         fun hashSHA256(input: String): String {
             val bytes = MessageDigest.getInstance("SHA-256").digest(input.toByteArray())
@@ -43,13 +51,25 @@ class Login : AppCompatActivity() {
 
 
             val pantallaPrincipal = Intent(this, Menu_PrincipalActivity::class.java)
-            GlobalScope.launch(Dispatchers.IO) {
+            CoroutineScope(Dispatchers.IO).launch {
                 //2- Creo una variable que contenga un PrepareStatement
                 //Se hace un select where el correo y la contraseña sean iguales a
                 //los que el usuario escribe
                 //Si el select encuentra un resultado es por que el usuario y contraseña si están
                 //en la base de datos, si se equivoca al escribir algo, no encontrará nada el select
                 val objConexion = ClaseConexion().cadenaConexion()
+
+                //Verificación de error en la conexión a la BD
+                if (objConexion == null) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(
+                            this@Login,
+                            "Usuario o contraseña incorrecta",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    return@launch
+                }
 
                 val contrasenaEncriptada = hashSHA256(txtContrasena.text.toString())
 
@@ -72,6 +92,11 @@ class Login : AppCompatActivity() {
 
                 }
             }
+        }
+
+        btnRegistrarse.setOnClickListener{
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
         }
         //Botón para ir a la pantalla inicial (Mientras no haya registro con google)
         val btn : Button = findViewById(R.id.btngoogle)
