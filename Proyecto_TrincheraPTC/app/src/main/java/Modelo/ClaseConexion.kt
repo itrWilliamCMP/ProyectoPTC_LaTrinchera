4bace22a79b0a37cd19af04790ea775331865934
danybrizz0077 <20230196@ricaldone.edu.sv>
2024-07-18 08:05:31 -0600

    arregle registrar

--- Proyecto_TrincheraPTC/app/src/main/java/Modelo/ClaseConexion.kt
+++ Proyecto_TrincheraPTC/app/src/main/java/Modelo/ClaseConexion.kt
@@ -7,9 +7,9 @@ import java.sql.DriverManager
 class ClaseConexion {
     fun cadenaConexion(): Connection?{
         try {
-            val url="jdbc:oracle:thin:@192.168.1.6:1521:xe"
-            val user="system"
-            val password="santiagoavs"
+            val url="jdbc:oracle:thin:@192.168.11.124:1521:xe"
+            val user="C##_PTCLT"
+            val password="ptccontraseña"
             val connection= DriverManager.getConnection(url, user, password)
 
             return connection
