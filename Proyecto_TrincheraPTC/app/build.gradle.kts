183af293f7f11dc1f6aaa84d9660c8d1a133dfeb
Santiago <santiago4v5@gmail.com>
2024-07-28 19:14:06 -0600

    Modificación en la pantalla registro: falta agregar un dato para la pfp del usuario en la bd + falta una línea de código para subir la imagen seleccionada por el usuario.

--- Proyecto_TrincheraPTC/app/build.gradle.kts
+++ Proyecto_TrincheraPTC/app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.firebase.common.ktx)
+    implementation(libs.firebase.storage.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
