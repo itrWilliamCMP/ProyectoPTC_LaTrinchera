7b96803ab9f7663661dc459012b331b241c4f646
danybrizz0077 <20230196@ricaldone.edu.sv>
2024-08-22 20:29:22 -0600

    Cambios pero falta solucionar un problema en cards

--- Proyecto_TrincheraPTC/app/src/main/java/Modelo/ClaseConexion.kt
+++ Proyecto_TrincheraPTC/app/src/main/java/Modelo/ClaseConexion.kt
@@ -7,11 +7,9 @@ import java.sql.DriverManager
 class ClaseConexion {
     fun cadenaConexion(): Connection?{
         try {
-<<<<<<< HEAD
+
             val url="jdbc:oracle:thin:@192.168.10.7:1521:xe"
-=======
-            val url="jdbc:oracle:thin:@10.10.4.159:1521:xe"
->>>>>>> 7e40b41934efe7816652d4b0d90a08a4e4524a4a
+
             val user="system"
             val password="ITR2024"
             val connection= DriverManager.getConnection(url, user, password)
