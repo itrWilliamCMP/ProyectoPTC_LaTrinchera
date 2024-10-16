00ed629eb297f7a8a248b58f3f9b73e5628a45a8
danybrizz0077 <20230196@ricaldone.edu.sv>
2024-10-16 07:30:47 -0600

    cambios de menu

--- Proyecto_TrincheraPTC/app/src/main/java/Modelo/ClaseConexion.kt
+++ Proyecto_TrincheraPTC/app/src/main/java/Modelo/ClaseConexion.kt
@@ -8,10 +8,10 @@ class ClaseConexion {
     fun cadenaConexion(): Connection?{
         try {
 
-            val url="jdbc:oracle:thin:@192.168.1.10:1521:xe"
+            val url="jdbc:oracle:thin:@192.168.235.124:1521:xe"
 
-            val user="SYSTEM"
-            val password="ITR2024"
+            val user="TRINCHE_PTC"
+            val password="TRINCHE_PTC"
             val connection= DriverManager.getConnection(url, user, password)
 
             return connection
